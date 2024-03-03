06aa8efaff6af74fac9a6eb0e3634488436c144d
AlexeyNelyubov <ANelyubov93@gmail.com>
2024-03-03 22:07:06 +0300

    project setup

---
+++ src/components/TheMain.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const a = '2'
+</script>
+
+<template>
+    <div class="main">
+        gfjhgjgh
+    </div>
+  </template>
+  
+  <style scoped>
+  
+  </style>
---
+++ src/components/TheSwitch.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+// defineProps<{
+//   msg: string
+// }>()
+</script>
+
+<template>
+  <div class="swith">
+    thgfhjdfhgg
+  </div>
+</template>
+
+<style scoped>
+
+</style>
